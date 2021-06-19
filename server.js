1a980b6ec607c380da634c8d9076dbe4ddf8f2a2
Raja halder <deepkexz@gmail.com>
2021-06-19 21:55:51 +0530

    here i am completed , login , register , order , empty , page ..

--- server.js
+++ server.js
@@ -7,17 +7,32 @@ const expressLayouts =require('express-ejs-layouts');
 const PORT = process.env.PORT  || 3000;
 
 
-app.get("/",(req,res)=>{
-    res.render("home");
-})
+
 app.use(express.static('public'));
 app.use(expressLayouts);
 app.set('views' , path.join(__dirname,"./resource/views"))
 app.set('view engine', 'ejs');
 
 
-
-
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+app.get('/order',(req,res)=>{
+  res.render('customar/some_item');
+})
+app.get('/card',(req,res)=>{
+    res.render('customar/card')
+    })
+//auth login
+    app.get('/register',(req,res)=>{
+        res.render('auth/singup')
+        })
+        // auth register
+        app.get('/login',(req,res)=>{
+            res.render('auth/login');
+            })
+        
+    
 app.listen(PORT ,()=>{
     console.log('server is working ',`${PORT}`);
 });
